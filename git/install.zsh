80999003c486f900c65ed423ae36fa1f79b84aaa
Richard McHorgh <richardmchorgh@fragile.co>
2025-04-16 06:34:38 -0700

    dude pls just show me the touchid thing

--- git/install.zsh
+++ git/install.zsh
@@ -1,6 +1,4 @@
 git config --global user.name "Richard McHorgh"
 git config --global user.email "richardmchorgh@fragile.co"
 git config --global commit.gpgsign true
-git config --global user.signingkey 9005A1A9
-YOURKEY=$(git config -l | grep signingkey | cut -d"=" -f 2)
-gpg -K --keyid-format SHORT | grep $YOURKEY -A3
+git config --global user.signingkey $SIGNING_KEY
